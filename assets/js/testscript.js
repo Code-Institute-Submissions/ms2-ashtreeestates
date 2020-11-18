b9ecfc83dd6ea870b82f10a71e340b0433e88a80
adamdelancey <adamdelancey@gmail.com>
2020-11-18 11:35:53 +0000

    added explore local area section, need to add remove marker function

--- assets/js/testscript.js
+++ assets/js/testscript.js
@@ -1,40 +1,17 @@
 $(document).ready(function () {  
 
-
-const contentString =    
-        "<h4 class='info-head'>St George's Road</h4><p><i class='fas fa-bed'></i> 2 &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 525 each pcm </p>"  
-
-let pos;
-let map;
-const myLatLng = { lat: 51.4510, lng: -2.6073 };
-
-
-function initMap() {
-    // Set the default location 
-        pos = myLatLng;
-        map = new google.maps.Map(document.getElementById('explore-map'), {
-            center: pos,
-            zoom: 15
-        });
-
-    // Set the marker for this property       
-    const marker = new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "St George's Road",
-  });
-
-    // Create info window
-    const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });  
-  
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
-}
-  
-  let currentInfoWindow;  
+  let pos;
+  let map;
+  let currentInfoWindow;
+  function initMap() {
+    // Set the default location and initialize all variables
+    pos = { lat: 51.4510, lng: -2.6073 };
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: pos,
+      zoom: 15,
+    });
+    
+  }
 
   function getNearbyPlaces(position, placetype) {
     let request = {
@@ -110,10 +87,6 @@ $("#cafeRadio").click(function(){
 
 $("#pubRadio").click(function(){
     getNearbyPlaces(pos, "pub");
-})
-
-$("#restaurantRadio").click(function(){
-    getNearbyPlaces(pos, "restaurant");
 })
 
   initMap();
