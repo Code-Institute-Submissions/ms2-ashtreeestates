0001a9d3596b5a72ba17fea18ff4cc441dfa9280
adamdelancey <adamdelancey@gmail.com>
2020-11-19 10:24:34 +0000

    added clear map button and added function to clear map when pub/cafe etc is clicked

--- assets/js/explore-map.js
+++ assets/js/explore-map.js
@@ -1,51 +1,51 @@
-$(document).ready(function () {  
+$(document).ready(function () {
+  const contentString =
+    "<h4 class='info-head'>St George's Road</h4><p><i class='fas fa-bed'></i> 2 &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 525 each pcm </p>";
 
+  let pos;
+  let map;
+  const myLatLng = { lat: 51.451, lng: -2.6073 };
+  let markers = [];
 
-const contentString =    
-        "<h4 class='info-head'>St George's Road</h4><p><i class='fas fa-bed'></i> 2 &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 525 each pcm </p>"  
+  function initMap() {
+    // Set the default location
+    pos = myLatLng;
+    map = new google.maps.Map(document.getElementById("explore-map"), {
+      center: pos,
+      zoom: 15,
+    });
+    
 
-let pos;
-let map;
-const myLatLng = { lat: 51.4510, lng: -2.6073 };
-
-
-function initMap() {
-    // Set the default location 
-        pos = myLatLng;
-        map = new google.maps.Map(document.getElementById('explore-map'), {
-            center: pos,
-            zoom: 15
-        });
-
-    // Set the marker for this property       
+    // Set the marker for this property
     const marker = new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "St George's Road",
-  });
+      position: myLatLng,
+      map,
+      title: "St George's Road",
+    });
 
     // Create info window
     const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });  
-  
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
-}
-  
-  let currentInfoWindow;  
+      content: contentString,
+    });
 
-  function getNearbyPlaces(position, placetype) {
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+  }
+
+  let currentInfoWindow;
+  
+  // Sets the map on all markers in the array.
+
+
+
+  function getNearbyPlaces(position, placeType) {
     let request = {
       location: position,
       rankBy: google.maps.places.RankBy.DISTANCE,
-      keyword: placetype,
-      
+      keyword: placeType,
+    };
 
-          };
-
-          
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, nearbyCallback);
   }
@@ -56,69 +56,102 @@ function initMap() {
       createMarkers(results);
     }
   }
+  function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    console.log(markers);
+  setMapOnAll(null);
+}
+
 
   function createMarkers(places) {
-    places.forEach(place => {
-    let marker = new google.maps.Marker({
+    places.forEach((place) => {
+      let marker = new google.maps.Marker({
         position: place.geometry.location,
         map: map,
         title: place.name,
-        icon: {                               url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"                           }
-    });
+        icon: { url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png" },
+        
+      });
 
-    /* TODO: Step 4B: Add click listeners to the markers */
-    google.maps.event.addListener(marker, 'click', () => {
-    let request = {
-    placeId: place.place_id,
-    fields: ['name', 'formatted_address', 'geometry', 'rating',
-        'website', 'photos']
-    };
+      markers.push(marker);
 
-    /* Only fetch the details of a place when the user clicks on a marker.
-    * If we fetch the details for all place results as soon as we get
-    * the search response, we will hit API rate limits. */
-    service.getDetails(request, (placeResult, status) => {
-    showDetails(placeResult, marker, status)
-    });
-});
+      /* TODO: Step 4B: Add click listeners to the markers */
+      google.maps.event.addListener(marker, "click", () => {
+        let request = {
+          placeId: place.place_id,
+          fields: [
+            "name",
+            "formatted_address",
+            "geometry",
+            "rating",
+            "website",
+            "photos",
+          ],
+        };
 
-function showDetails(placeResult, marker, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-    let placeInfowindow = new google.maps.InfoWindow();
-    placeInfowindow.setContent('<div><strong>' + placeResult.name +
-        '</strong><br>' + 'Rating: ' + placeResult.rating + '</div>');
-    placeInfowindow.open(marker.map, marker);
+        /* Only fetch the details of a place when the user clicks on a marker.
+         * If we fetch the details for all place results as soon as we get
+         * the search response, we will hit API rate limits. */
+        service.getDetails(request, (placeResult, status) => {
+          showDetails(placeResult, marker, status);
+        });
+      });
 
-    if (currentInfoWindow){
-       currentInfoWindow.close(); 
-    }
+      function showDetails(placeResult, marker, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          let placeInfowindow = new google.maps.InfoWindow();
+          placeInfowindow.setContent(
+            "<div><strong>" +
+              placeResult.name +
+              "</strong><br>" +
+              "Rating: " +
+              placeResult.rating +
+              "</div>"
+          );
+          placeInfowindow.open(marker.map, marker);
 
-    currentInfoWindow = placeInfowindow;
+          if (currentInfoWindow) {
+            currentInfoWindow.close();
+          }
+
+          currentInfoWindow = placeInfowindow;
+        } else {
+          console.log("showDetails failed: " + status);
+        }
+      }
+    });    
     
-    } else {
-    console.log('showDetails failed: ' + status);
-    }
-}
+  }
 
+ 
 
-  
-    });
-
-    
-}
-
-$("#cafeRadio").click(function(){
+  $("#cafeRadio").click(function () {
+      clearMarkers();
     getNearbyPlaces(pos, "cafe");
-})
+  });
 
-$("#pubRadio").click(function(){
+  $("#pubRadio").click(function () {
+      clearMarkers();
     getNearbyPlaces(pos, "pub");
-})
+  });
 
-$("#restaurantRadio").click(function(){
+  $("#restaurantRadio").click(function () {
+      clearMarkers();
     getNearbyPlaces(pos, "restaurant");
-})
+  });   
+
+   $("#clear-btn").click(function () {
+    clearMarkers();
+  });   
+
+
 
   initMap();
 });
-
