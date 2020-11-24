d232266b0a7a882f85bd2ba7b5b3b994e3beb912
adamdelancey <adamdelancey@gmail.com>
2020-11-24 14:12:33 +0000

    combined all property listing js pages into one

--- assets/js/properties-listing.js
+++ assets/js/properties-listing.js
@@ -1,11 +1,88 @@
+let properties = {
+
+    stgeorges: {
+        lat: 51.4510,
+        lng: -2.6073,        
+        contentString: "<h4>St George's Road, Hotwell</h4><i class='fas fa-bed'></i> 2  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 525 each pcm"
+    },
+
+    abbotsford: { 
+        lat: 51.4642,
+        lng: -2.6062,       
+        contentString: "<h4 class='info-head'>Abbotsford Road, Cotham</h4><p><i class='fas fa-bed'></i> 2  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 605 each pcm </p>"
+        },
+    
+    richmond: { 
+        lat: 51.4568,
+        lng: -2.6121,   
+        contentString: "<h4 class='info-head'>Richmond Hill Avenue, Clifton</h4><p><i class='fas fa-bed'></i> 3  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 570 each pcm </p>"
+        },   
+
+    ashgrove: { 
+        lat: 51.4665,
+        lng: -2.6098,
+        contentString: "<h4 class='info-head'>Ashgrove Road, Redland</h4><p><i class='fas fa-bed'></i> 3  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 565 each pcm </p>"
+        }, 
+
+    smhl: { 
+        lat: 51.4603,
+        lng: -2.6016,
+        contentString: "<h4 class='info-head'>Saint Michael's Hill Lower, Cotham</h4><p><i class='fas fa-bed'></i> 3  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 580 each pcm </p>"
+        }, 
+
+    berkeley: { 
+        lat: 51.4769,
+        lng: -2.5974,       
+        contentString: "<h4>Berkeley Avenue, Redland</h4><i class='fas fa-bed'></i> 4  &emsp;<i class='fas fa-bath'></i> 2 &emsp; <i class='fas fa-pound-sign'></i> 525 each pcm"
+        },
+
+    rosebury: { 
+        lat: 51.4537,
+        lng: -2.6082,      
+        contentString: "<h4>Rosebury Terrace, Clifton</h4><i class='fas fa-bed'></i> 4  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 570 each pcm"
+        },
+
+    dowry: { 
+        lat: 51.4517,
+        lng: -2.6202,
+        contentString: "<h4>Dowry Road, Clifton Wood</h4><i class='fas fa-bed'></i> 4  &emsp;<i class='fas fa-bath'></i> 1 &emsp; <i class='fas fa-pound-sign'></i> 555 each pcm"
+        },
+
+    smhu: { 
+        lat: 51.4603,
+        lng: -2.6016,
+        contentString: "<h4>Saint Michael's Hill Upper, Cotham</h4><i class='fas fa-bed'></i> 5  &emsp;<i class='fas fa-bath'></i> 2 &emsp; <i class='fas fa-pound-sign'></i> 560 each pcm"
+        },
+
+    high: { 
+        lat: 51.4546,
+        lng: -2.5927,
+        contentString: "<h4>High Street, City Centre</h4><i class='fas fa-bed'></i> 6  &emsp;<i class='fas fa-bath'></i> 2 &emsp; <i class='fas fa-pound-sign'></i> 545 each pcm"
+        },
+
+    pipe: { 
+        lat: 51.4545,
+        lng: -2.5989,
+        contentString: "<h4>Pipe Lane, City Centre</h4><i class='fas fa-bed'></i> 7  &emsp;<i class='fas fa-bath'></i> 3 &emsp; <i class='fas fa-pound-sign'></i> 565 each pcm"
+        } 
+    
+}
+
+
+
 $(document).ready(function () {
-  const contentString =
-    "<h4 class='info-head'>High Street, City Centre</h4><p><i class='fas fa-bed'></i> 6  &emsp;<i class='fas fa-bath'></i> 2 &emsp; <i class='fas fa-pound-sign'></i> 545 each pcm </p>";
+
+   let propertyName = $("#property").attr("name")
+    console.log(propertyName)
+  const contentString = properties[propertyName].contentString;
+
+    
 
   let pos;
   let map;
-  const myLatLng = {lat: 51.4546,
-            lng: -2.5927};
+  const myLatLng = { lat: properties[propertyName].lat,
+            lng: properties[propertyName].lng
+        };
   let markers = [];
 
   function initMap() {
@@ -21,8 +98,11 @@ $(document).ready(function () {
     const marker = new google.maps.Marker({
       position: myLatLng,
       map,
-      title: "High Street",
+     
+      
     });
+
+    
 
     // Create info window
     const infowindow = new google.maps.InfoWindow({
@@ -65,8 +145,7 @@ $(document).ready(function () {
 
 
 // Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-    console.log(markers);
+function clearMarkers() {    
   setMapOnAll(null);
 }
 
